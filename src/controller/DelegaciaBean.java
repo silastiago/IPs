ef078b75d756533da4d3048e1ae69b390c7a9dbe
Sinf02 <Sinf02>
2017-01-10 14:44:00 -0300

    Mudando botao editar e retirando alguns jars desnecessarios

--- src/controller/DelegaciaBean.java
+++ src/controller/DelegaciaBean.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
